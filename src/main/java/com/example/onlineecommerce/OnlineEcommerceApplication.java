2ef78b711df87249e896b8872777bf82d251994c
saban <isuleimanibrahim@gmail.com>
2022-12-23 23:48:23 +0100

    first commit

---
+++ src/main/java/com/example/onlineecommerce/OnlineEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.example.onlineecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineEcommerceApplication.class, args);
+	}
+
+}
